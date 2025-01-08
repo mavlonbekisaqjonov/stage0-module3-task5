fc1d9cfbf177b37b2b5d7e357b9f29419a84e3c7
Mavl0nbek <mavlonbekisaqjonov222@gmail.com>
2025-01-08 19:09:05 +0500

    done

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float out = minutes * 60;
+        System.out.println(out);
     }
 }
